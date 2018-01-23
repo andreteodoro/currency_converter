8c5838933481eebbe464245d95590cf73c251f4d
André Luiz Teodoro <andre.teodoro@eldorado.org.br>
2018-01-23 17:36:09 -0200

    Fixing error with field name

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "Convert Currency Process", :type => :feature do
   it "convert value" do
     visit '/'
-    within("#converter_form") do
+    within("#exchange_form") do
       select('EUR', from: 'currency_from')
       select('USD', from: 'currency_to')
       fill_in 'amount', with: '10'
     end
-    click_button 'CONVERTER'
+    click_button 'CONVERT'
 
     expect(page).to have_content("value")
   end
